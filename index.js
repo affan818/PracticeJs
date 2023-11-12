ff9cb20425a65e63717ebeab55c49d339d664ac1
affan818 <ansariaffan818@gmail.com>
2023-11-12 06:40:46 +0530

    add new line

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const myFirstArry = ["affan", "arhan", "ahmad"];
-console.log(myFirstArry);
+// console.log(myFirstArry);
 const secondArry = ["ansari"];
 
+// let pushArry = myFirstArry.push(secondArry);
+// console.log(pushArry);
+
 const newArry = myFirstArry.concat(secondArry);
-console.log(newArry)
+console.log(newArry);
