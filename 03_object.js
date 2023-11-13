a29e0e7a757673178702a697bb51fd9c24f4e91b
affan818 <ansariaffan818@gmail.com>
2023-11-13 19:34:42 +0530

    Object.freez method

--- 03_object.js
+++ 03_object.js
@@ -16,4 +16,25 @@ lastLoginDays:["monday","saturday"]
 // console.log(JsUser["email"])
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
-console.log(JsUser)
+
+// you can change the value by accessing data out of object
+JsUser["email"]="affanahmad@google.com"
+console.log(JsUser["email"])
+
+
+// you can freez the object for want any other change this
+// Object.freeze(JsUser)
+    // JsUser.age=25
+// console.log(JsUser)
+
+JsUser.greeting = function(){
+    console.log('hii Affan ');
+}
+
+JsUser.greetingTwo = function(){
+    console.log("how are you")
+}
+console.log(JsUser.greeting())
+console.log(JsUser.greetingTwo())
+
+
