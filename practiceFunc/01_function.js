4098a6ab61a6b1ee14b4caa49fc5c680fd3c8c1f
affan818 <ansariaffan818@gmail.com>
2023-11-19 07:12:12 +0530

    practiceFunction

---
+++ practiceFunc/01_function.js
@@ -0,0 +1,39 @@
+
+// function addTwoNumber (num,num) {
+//     console.log(num + num)
+// }
+
+// addTwoNumber(2,2)
+
+// second type 
+
+function addTwoNumber (num,num) {
+//  let result = num + num
+//  console.log(result)
+// return result
+return num + num
+}
+
+let result = addTwoNumber(2,8)
+// console.log("Result",result)
+
+function loginUserMessage (username) {
+    // tripple equal sign method 
+    // if (username===undefined){
+    //     console.log('please enter a name')
+    //     return 
+    // }
+
+    // ! sign methon this sign convert true to false or false to true
+    if (!username){
+        console.log('please enter a name')
+        return
+    }
+return ` ${username} just loggedin`
+}
+
+// console.log(loginUserMessage('affan'))
+console.log(loginUserMessage())
+
+
+
