af762c8e8b2273b337b88d8d1ba81a730a75b95a
affan818 <ansariaffan818@gmail.com>
2024-02-12 17:50:52 +0530

    update my myClasses

--- 10_classes-and_loops/myClasses.js
+++ 10_classes-and_loops/myClasses.js
@@ -1,45 +1,45 @@
 // Javascript is prototype based language after ES6 Js has classes
 
-// class User {
-//   constructor(username, email, password) {
-//     this.username = username;
-//     this.password = password;
-//     this.email = email;
-//   }
-//   encryptPassword() {
-//     return `${this.password}abc`;
-//   }
-//   greetings() {
-//     console.log(
-//       `Wellocom ${this.username} your password is ${this.encryptPassword()}`
-//     );
-//   }
-// }
+class User {
+  constructor(username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+  encryptPassword() {
+    return `${this.password}@abc`;
+  }
 
-// const affan = new User("affan", "affan@fb.com", 123);
+  greetings() {
+    return `wellcome ${
+      this.username
+    } your password is ${this.encryptPassword()}`;
+  }
+}
+const user1 = new User("affan", "ansri@fb.com", 456);
 
-// affan.greetings();
-// console.log(affan.encryptPassword());
+console.log(user1);
+console.log(user1.encryptPassword());
+console.log(user1.greetings());
 
 //////////behind the scene//////////
 
-function User(username, email, password) {
-  this.username = username;
-  this.password = password;
-  this.email = email;
-}
+// function User(username, email, password) {
+//   this.username = username;
+//   this.email = email;
+//   this.password = password;
+// }
 
-User.prototype.encryptPassword = function () {
-  return `${this.password}abc`;
-};
+// User.prototype.encryptPassword = function () {
+//   return `${this.password}@abc`;
+// };
 
-User.prototype.greetings = function () {
-  console.log(
-    `wellcome ${this.username} your password is ${this.encryptPassword()}`
-  );
-};
+// User.prototype.greetings = function () {
+//   return `Wellcome ${this.username} your password is ${this.encryptPassword()}`;
+// };
 
-const affan = new User("affan", "affan@fb.com", 123);
+// const user1 = new User("affan", "ansari@fb.com", 456);
 
-affan.greetings();
-console.log(affan.encryptPassword());
+// console.log(user1);
+// console.log(user1.encryptPassword());
+// console.log(user1.greetings());
